afe736ac7c064bb0a612872869c1f4b3ab04b526
thatging3rkid <chenl16039@gmail.com>
2016-01-28 23:43:17 -0500

    added test version

---
+++ AutoConvertServer.py
@@ -0,0 +1,54 @@
+# AutoConvertServer v0.1
+# By: thatging3rkid
+
+import os
+import time
+import logging
+import threading
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-9s) %(message)s',)
+
+# Some global pre-defs
+global threads
+threads = []
+global inputDir
+numThreads = 2
+vlcDir = "" + os.getcwd() + "\\vlc\\"
+cd = os.getcwd()
+
+
+# Main function
+def main():
+	if (len(inputDir) == 0):
+		time.sleep(1)
+		logging.debug("sleeping")
+	else:
+		s = threading.Semaphore(numThreads)
+		for dirCount in range(len(inputDir)):
+			t = threading.Thread(target=convertThread, args=(dirCount, s))
+			threads.append(t)
+			t.start()
+				
+
+
+# Function that is threaded that makes the VLC call
+def convertThread(index , s):
+	logging.debug("in queue")
+	with s:
+		logging.debug("starting")			
+		currentFile = inputDir[index]
+		newFile =  currentFile[0: -4] + ".mp3"
+		inFile = "\"" + cd + "\\input\\" + currentFile + "\""
+		outFileMP = "\"" + cd + "\\output\\mp3\\" + newFile + "\""
+		outFileSc = "\"" + cd + "\\output\\source\\" + currentFile + "\""
+		callSuccess = subprocess.call("" + vlcDir + "vlc.exe --play-and-exit -I dummy --dummy-quiet " + inFile + " --sout=#transcode{" + \
+			"acodec=mp3,vcodec=dummy}:standard{access=file,mux=raw,dst=" + outFileMP + "}")
+		logging.debug("in thread")
+		os.system("move " + inFile + " " + outFileSc)
+	pass
+
+inputDir = os.listdir("" + os.getcwd() + "\\input")
+main()
+#make infinite loop in final revision
---
+++ startup.bat
@@ -0,0 +1,8 @@
+:: Batch script for running AutoConvertServer
+@echo off
+
+:: py.exe makes a cli screen
+:: pyw.exe makes no cli screen
+:: recommended: pyw.exe
+
+"%cd%\py\python.exe" AutoConvertServer.py
